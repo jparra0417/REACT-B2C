40b06671e142238f17e56c58efb459aa725202c5
Julian Parra <jparra0417@gmail.com>
2020-05-11 16:23:42 +1200

    JP - Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer";
 
 export const App = () => {
   return (
@@ -9,6 +10,7 @@ export const App = () => {
       <BrowserRouter>
         <Header />
         <Content />
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="b2c-footer">
+            &copy; 2020 <br />
+            Powered by <a href="https://jparra.web.app">Julian Parra</a>            
+        </div>
+    )
+}
+
+export default Footer
